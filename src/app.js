ffe78f79363d27048e7eeee2f34cfb190168c7c9
StandWater <sutiong123@gmail.com>
2022-12-28 03:49:38 -0500

    Updated JS

--- src/app.js
+++ src/app.js
@@ -19,22 +19,28 @@ function formatDate(timestamp) {
 }
 
 function displayTemp(response) {
-  let tempElement = document.querySerlector("#temp");
-  let cityElement = document.querySerlector("#city");
-  let descriptionElement = document.querySerlector("#description");
-  let humidityElement = document.querySerlector("#humidity");
-  let windElement = document.querySerlector("#wind");
-  let dateTimeElement = document.querySerlector("#dateTime");
+  let tempElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateTimeElement = document.querySelector("#dateTime");
+  let iconElement = document.querySelector("#icon");
+
   tempElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.main.wind.speed);
   dateTimeElement.innerHTML = "Friday 05:00";
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = "9bb8a8720baa1a749633906d229159a5";
-let city = "New York";
+let city = "Lisbon";
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
 
